3f32e9e15da6564ad9a5eb6fae4b913f2c965cfb
Shliskenstickin <deathwalker@mail.ru>
2021-03-06 14:14:14 +0300

    correcting

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
         int threshold = 1000;
         if (transfer >= threshold) {
             int bonus = transfer / bonusStep;
-            balance = balance + transfer + bonus;
+            balance += transfer + bonus;
             System.out.println("Пополнение на " + transfer + " рублей");
             System.out.println("Так держать! Вы получили " + bonus + " бонусных рублей на свой счет.");
             System.out.println("Сумма вашего баланса " + balance + " рублей. Скорей беги звонить маме по межгороду и возвращайся назад!\n");
         } else {
-            balance = balance + transfer;
+            balance += transfer;
             System.out.println("Пополнение на " + transfer + " рублей");
             System.out.println("Ты получаешь 0 бонусов :(");
             System.out.println("Баланс твоего счета всего лишь " + balance + " жалких рублей. В следующий раз старайся лучше!\n");
